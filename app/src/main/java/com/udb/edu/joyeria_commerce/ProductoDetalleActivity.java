89ec13e2de4d294b7e5b03df545bb155148edee8
CESCO <cescovalledev@gmail.com>
2022-04-24 15:41:16 -0600

    En teoria deberia funcionar, pero no se porque

--- app/src/main/java/com/udb/edu/joyeria_commerce/ProductoDetalleActivity.java
+++ app/src/main/java/com/udb/edu/joyeria_commerce/ProductoDetalleActivity.java
@@ -1,14 +1,58 @@
 package com.udb.edu.joyeria_commerce;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
-public class ProductoDetalleActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.List;
+
+public class ProductoDetalleActivity extends AppCompatActivity implements TallasAdapter.TallaSeleccionada{
+
+    RecyclerView rcl_tallas;
+    RecyclerView rcl_colores;
+
+    List<TallasModel> tallasModelList=new ArrayList<>();
+
+    TallasAdapter tallasAdapter;
+
+    String[] tallas={"3","3.5","4","4.5","5","5.5","6"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_producto_detalle);
+
+        rcl_tallas=findViewById(R.id.rclview_tallas);
+        rcl_colores=findViewById(R.id.rclview_colores);
+
+        LinearLayoutManager linearLayoutManager=new LinearLayoutManager(this);
+        linearLayoutManager.setOrientation(LinearLayoutManager.HORIZONTAL);
+
+        for (String s:tallas) {
+            TallasModel tallasModel=new TallasModel(s);
+
+            tallasModelList.add(tallasModel);
+        }
+
+        rcl_tallas.setLayoutManager(linearLayoutManager);
+        rcl_tallas.addItemDecoration(new DividerItemDecoration(this,DividerItemDecoration.VERTICAL));
+
+        rcl_colores.setLayoutManager(linearLayoutManager);
+        rcl_colores.addItemDecoration(new DividerItemDecoration(this,DividerItemDecoration.VERTICAL));
+
+        tallasAdapter=new TallasAdapter(tallasModelList,this);
+
+        rcl_tallas.setAdapter(tallasAdapter);
+        rcl_colores.setAdapter(tallasAdapter);
+    }
+
+    @Override
+    public void tallaSeleccionada(TallasModel tallasModel){
+        Toast.makeText(this, tallasModel.getTallasModel(), Toast.LENGTH_SHORT).show();
     }
 }
--- app/src/main/java/com/udb/edu/joyeria_commerce/TallasAdapter.java
+++ app/src/main/java/com/udb/edu/joyeria_commerce/TallasAdapter.java
@@ -2,23 +2,27 @@ package com.udb.edu.joyeria_commerce;
 
 import android.content.Context;
 import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Filter;
 import android.widget.Filterable;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.List;
-import java.util.logging.Filter;
+import java.util.logging.LogRecord;
 
 public class TallasAdapter extends RecyclerView.Adapter<TallasAdapter.TallasAdapterVh> implements Filterable {
     private List<TallasModel> tallasModelList;
     private Context context;
     private TallaSeleccionada tallaSeleccionada;
-    
+
     public TallasAdapter(List<TallasModel> tallasModelList,TallaSeleccionada tallaSeleccionada){
         this.tallaSeleccionada=tallaSeleccionada;
         this.tallasModelList=tallasModelList;
+        this.tallaSeleccionada=tallaSeleccionada;
     }
 
     @NonNull
@@ -26,6 +30,38 @@ public class TallasAdapter extends RecyclerView.Adapter<TallasAdapter.TallasAdap
     public TallasAdapter.TallasAdapterVh onCreateViewHolder(@NonNull ViewGroup parent, int viewType){
         context = parent.getContext();
 
-        return new TallasAdapterVh(LayoutInflater.from(context).inflate(R.layout.activity_producto_detalle,null));
+        return new TallasAdapterVh(LayoutInflater.from(context).inflate(R.layout.fragment_recycler_view_tallas,null));
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull TallasAdapter.TallasAdapterVh holder,int position){
+        TallasModel tallasModel=tallasModelList.get(position);
+
+        String talla=tallasModel.getTallasModel();
+
+        holder.tvTalla.setText(talla);
+    }
+
+    @Override
+    public int getItemCount(){
+        return tallasModelList.size();
+    }
+
+    @Override
+    public Filter getFilter() {
+        return null;
+    }
+
+
+    public interface TallaSeleccionada{
+        void tallaSeleccionada(TallasModel tallasModel);
+    }
+
+    public class TallasAdapterVh extends RecyclerView.ViewHolder{
+        TextView tvTalla;
+        public TallasAdapterVh(@NonNull View itemView){
+            super(itemView);
+            tvTalla=itemView.findViewById(R.id.prefix);
+        }
     }
 }
