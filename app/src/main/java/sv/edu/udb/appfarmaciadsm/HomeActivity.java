91502883f3de491509047966e4c624b0ba0c1a44
Ebony Gabriela Pena Gomez <ebgpena@bancoagricola.com.sv>
2022-04-24 12:13:21 -0600

    Cargando pantallas de registro y login

---
+++ app/src/main/java/sv/edu/udb/appfarmaciadsm/HomeActivity.java
@@ -0,0 +1,28 @@
+package sv.edu.udb.appfarmaciadsm;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+
+public class HomeActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+
+
+}
---
+++ app/src/main/java/sv/edu/udb/appfarmaciadsm/SplashActivity.java
@@ -0,0 +1,34 @@
+package sv.edu.udb.appfarmaciadsm;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        //Código para que la splashscreen se muestre por poco tiempo
+        Thread thread = new Thread(){
+            public void run(){
+                try {
+
+                    sleep(1575);
+                }
+                catch (Exception e){
+                    e.printStackTrace();
+                }
+                finally {
+                    Intent newIntent = new Intent(SplashActivity.this, HomeActivity.class);
+                    startActivity(newIntent);
+                    finish();
+                }
+            }
+
+        };
+        thread.start();
+    }
+}
