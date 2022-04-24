c8f622f46a0064a8597c163ad8b069531afca0f9
refv21 <killahvue@gmail.com>
2022-04-23 20:51:31 -0600

    Se agrega pantalla 6

---
+++ app/src/main/java/self/is/ccahill/com/au/shapelist/DetailActivity.java
@@ -0,0 +1,39 @@
+package self.is.ccahill.com.au.shapelist;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class DetailActivity extends AppCompatActivity
+{
+    Joyas selectedJoyas;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+        getSelectedJoyas();
+        setValues();
+
+    }
+
+    private void getSelectedJoyas()
+    {
+        Intent previousIntent = getIntent();
+        String parsedStringID = previousIntent.getStringExtra("id");
+        selectedJoyas = MainActivity.joyasList.get(Integer.valueOf(parsedStringID));
+    }
+
+    private void setValues()
+    {
+        TextView tv = (TextView) findViewById(R.id.joyasName);
+        ImageView iv = (ImageView) findViewById(R.id.joyasImage);
+
+        tv.setText(selectedJoyas.getName());
+        iv.setImageResource(selectedJoyas.getImage());
+    }
+}
---
+++ app/src/main/java/self/is/ccahill/com/au/shapelist/JoyasAdapter.java
@@ -0,0 +1,40 @@
+package self.is.ccahill.com.au.shapelist;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.List;
+
+public class JoyasAdapter extends ArrayAdapter<Joyas>
+{
+
+    public JoyasAdapter(Context context, int resource, List<Joyas> joyasList)
+    {
+        super(context,resource,joyasList);
+    }
+
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent)
+    {
+        Joyas joyas = getItem(position);
+
+        if(convertView == null)
+        {
+            convertView = LayoutInflater.from(getContext()).inflate(R.layout.joyas_cell, parent, false);
+        }
+        TextView tv = (TextView) convertView.findViewById(R.id.joyasName);
+        ImageView iv = (ImageView) convertView.findViewById(R.id.joyasImage);
+
+        tv.setText(joyas.getName());
+        iv.setImageResource(joyas.getImage());
+
+
+        return convertView;
+    }
+}
